2db5db19b210f520be3e11304cee6a609073e25e
Sam Lau <liuyaozhu13hao@163.com>
2015-09-22 07:48:37 +0800

    Create iOS project

---
+++ CustomView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomView
+//
+//  Created by Sam Lau on 9/22/15.
+//  Copyright © 2015 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomView
+//
+//  Created by Sam Lau on 9/22/15.
+//  Copyright © 2015 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
